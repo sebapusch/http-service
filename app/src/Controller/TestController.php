a5e57373f58d7cd2314543226817810163d58518
Sebastian Pusch <seba0808@gmail.com>
2021-09-01 20:30:12 +0200

    Initial commit

---
+++ app/src/Controller/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class TestController extends AbstractController
+{
+    public function test()
+    {
+        echo 'Hello World';
+    }
+}
